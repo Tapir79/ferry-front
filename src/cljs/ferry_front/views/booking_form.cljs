7432853077cf5c6f8d80cb057b5dc1b1e07fa201
eero3 <eero.siren@kolumbus.fi>
2018-12-02 21:41:11 +0200

    added font-awesome and icon to dropdowns

--- src/cljs/ferry_front/views/booking_form.cljs
+++ src/cljs/ferry_front/views/booking_form.cljs
@@ -1,9 +1,7 @@
 (ns ferry-front.views.booking-form
   (:require [re-frame.core :as re-frame]
-            [reagent.core :as reagent :refer [atom]]
-            [ferry-front.events :as events]
             [ferry-front.subs :as subs]
-            [stylefy.core :refer [use-style]]))
+            [stylefy.core :as stylefy]))
 
 (enable-console-print!)
 
@@ -11,14 +9,16 @@
   ())
 
 (def booking-form-style {:width "97.5%"})
+(def icon-style {:top "29px" :left "0.5em" :pointer-events "none"
+                 ::stylefy/media {{:max-width "576px"} {:top "9px"}}})
 
-(defn styled-select [id label placeholder options on-change-function]
-  [:div {:class "flex-grow flex-shrink mb-1 sm:m-1 w-full sm:w-auto"}
+(defn styled-select [id label placeholder options on-change-function icon]
+  [:div {:class "flex-grow flex-shrink mb-1 sm:m-1 w-full sm:w-auto relative"}
    [:label {:for id :class "hidden sm:block text-sm font-bold sm:pr-1 text-grey-darker"} label]
-   [:select {:id id :class "rounded py-2 px-1 bg-white w-full" :on-change on-change-function }
+   [:select {:id id :class "rounded py-2 pl-8 px-1 bg-white w-full" :on-change on-change-function }
     [:option {:class "text-grey-darker"} placeholder]
-    (options)
-    ]])
+    (options)]
+   [:i (stylefy/use-style icon-style {:class (str icon " " "absolute z-10 text-grey-darker")})]])
 
 (defn search-button []
   [:div {:class "flex-grow flex-shrink w-full sm:w-auto my-1 sm:m-1"}
@@ -38,11 +38,11 @@
       [:option {:key (:id stop) :value (:id stop)} (:name stop) ])))
 
 (defn form [lines stops]
-  [:div (use-style booking-form-style {:class "sm:w-full -ml-1 lg:ml-3 sm:mx-1 pb-1"})
+  [:div (stylefy/use-style booking-form-style {:class "sm:w-full -ml-1 lg:ml-3 sm:mx-1 pb-1"})
    [:form {:class "w-full flex flex-wrap items-center sm:items-end bg-blue-lighter rounded p-1 pb-0 sm:p-2 ml-2 mb-1"}
-    (styled-select "route-selection" "Route" "Select route" (get-lines-options lines) #(println "eka"))
-    (styled-select "from" "from" "Departure harbor" (get-stops-options stops) #(println "satama"))
-    (styled-select "to" "to" "Arrival harbor" (get-stops-options stops) #(println "arrival"))
+    (styled-select "route-selection" "Route" "Select route" (get-lines-options lines) #(println "eka") "fas fa-route")
+    (styled-select "from" "from" "Departure harbor" (get-stops-options stops) #(println "satama") "fas fa-anchor")
+    (styled-select "to" "to" "Arrival harbor" (get-stops-options stops) #(println "arrival") "fas fa-anchor")
     (search-button)]])
 
 (defn booking-form []
