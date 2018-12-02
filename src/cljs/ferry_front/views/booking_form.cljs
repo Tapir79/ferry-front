69405fe5978b2f065f5f7d2a85908762ae72bb31
eero3 <eero.siren@kolumbus.fi>
2018-12-02 12:32:26 +0200

    added button to booking form with responsive styles

--- src/cljs/ferry_front/views/booking_form.cljs
+++ src/cljs/ferry_front/views/booking_form.cljs
@@ -16,9 +16,14 @@
       ^{:key option} [:option option])
     ]])
 
+(defn search-button []
+  [:div {:class "flex-grow flex-shrink w-full sm:w-auto my-1 sm:m-1"}
+   [:button {:class "bg-blue hover:bg-blue-dark font-semibold text-white p-2 rounded w-full sm:w-4/5"} "Search"]])
+
 (defn booking-form []
   [:div (use-style booking-form-style {:class "sm:w-full -ml-1 lg:ml-3 sm:mx-1 pb-1"})
-    [:form {:class "w-full flex flex-wrap items-center bg-blue-lighter rounded p-1 pb-0 sm:p-2 ml-2 mb-1"}
+    [:form {:class "w-full flex flex-wrap items-center sm:items-end bg-blue-lighter rounded p-1 pb-0 sm:p-2 ml-2 mb-1"}
       (styled-select "route-selection" "Route" "Select route" (list "eka" "toka"))
       (styled-select "from" "from" "Departure harbor" (list "satama1" "satama2"))
-      (styled-select "to" "to" "Arrival harbor" (list "satama1" "satama2"))]])
+      (styled-select "to" "to" "Arrival harbor" (list "satama1" "satama2"))
+     (search-button)]])
