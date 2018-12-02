7b275466a4485370bf710b41bbbcb865dc44a96f
eero3 <eero.siren@kolumbus.fi>
2018-12-02 12:00:21 +0200

    fixed navigation size on small devices

--- src/cljs/ferry_front/views/navigation.cljs
+++ src/cljs/ferry_front/views/navigation.cljs
@@ -3,11 +3,11 @@
   (defn main-navigation []
     [:nav {:class "p-1 mb-1 sm:ml-2 lg:ml-4"}
      [:ul {:class "list-reset flex text-grey-light font-medium"}
-      [:li {:class "mr-6 "}
+      [:li {:class "mr-2 sm:mr-6"}
        [:a {:class "text-white cursor-pointer"} "Booking"]]
-      [:li {:class "mr-6"}
+      [:li {:class "mr-2 sm:mr-6"}
        [:a {:class "cursor-pointer hover:text-white"} "Timetables"]]
-      [:li {:class "mr-6"}
+      [:li {:class "mr-2 sm:mr-6"}
        [:a {:class "cursor-pointer hover:text-white"} "Analysis"]]
-      [:li {:class "mr-6"}
+      [:li {:class "mr-2 sm:mr-6"}
        [:a {:class "cursor-pointer hover:text-white"} "Something"]]]])
