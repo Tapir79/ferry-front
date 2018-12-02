70e1f7e255e392c25fb93e886cfc833680e0db8b
eero3 <eero.siren@kolumbus.fi>
2018-12-02 12:05:26 +0200

    added flex-wrap on nav

--- src/cljs/ferry_front/views/navigation.cljs
+++ src/cljs/ferry_front/views/navigation.cljs
@@ -2,7 +2,7 @@
 
   (defn main-navigation []
     [:nav {:class "p-1 mb-1 sm:ml-2 lg:ml-4"}
-     [:ul {:class "list-reset flex text-grey-light font-medium"}
+     [:ul {:class "list-reset flex text-grey-light font-medium flex flex-wrap"}
       [:li {:class "mr-2 sm:mr-6"}
        [:a {:class "text-white cursor-pointer"} "Booking"]]
       [:li {:class "mr-2 sm:mr-6"}
