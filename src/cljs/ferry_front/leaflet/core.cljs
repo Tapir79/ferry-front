3a419d7528e916af6d9fbc58ab0347b462cbf8b4
tapir79 <unnu100s>
2018-12-06 15:44:59 +0200

    points and popups

--- src/cljs/ferry_front/leaflet/core.cljs
+++ src/cljs/ferry_front/leaflet/core.cljs
@@ -15,30 +15,27 @@
         view (:view mapspec)
         zoom (:zoom mapspec)
         line-weight (:weight mapspec)
-        line (:line mapspec)]
+        ]
 
     #_(println "view" line-weight)
     (.setView leaflet (clj->js @view) @zoom)
 
     (doseq [{:keys [type url] :as layer-spec} (:layers mapspec)]
-      #_(println "type" type)
+      #_(println "url" url)
+      (println "mapspec:" mapspec)
       (let [layer (case type
                     :tile (js/L.tileLayer
                             url
                             (clj->js {:attribution (:attribution layer-spec)})
                             )
-                    :wms (js/L.tileLayer.wms
-                           url
-                           (clj->js {:format      "image/png"
-                                     :fillOpacity 1.0
-                                     }))
                     :json (js/L.geoJson
                             (.parse js/JSON url)
                             (clj->js {:style
                                       {:color    "#400080"
                                        :linejoin "round"
-                                       :weight   line
-                                       :opacity  0.50}})))]
+                                       :weight   line-weight
+                                       :opacity  0.50}}))
+                    )]
         ;;(.log js/console "L.tileLayer = " layer)
         (.addTo layer leaflet)))
 
@@ -50,7 +47,7 @@
                             (clj->js {:style
                                       {:color    color
                                        :linejoin linejoin
-                                       :weight   line
+                                       :weight   line-weight
                                        :opacity  0.50}})))]
         ;;(.log js/console "L.tileLayer = " layer)
         (.addTo layer leaflet)))
@@ -97,36 +94,67 @@
         this-state (reagent/state this)
         mapspec (:mapspec this-state)
         base-jsons (:base-jsons mapspec)
+        stops (:stops mapspec)
         old-highlight-json (:jsons mapspec)
         highlight-json (:jsons (second old-state))]
 
     #_#_#_#_leaflet (:leaflet (reagent/state this))
 
-    previous-state (reagent/state old-state)
+        previous-state (reagent/state old-state)
     #_(doseq [x old-state]
         (println "old:state x:" x))
 
-    (println "base-jsons" base-jsons)
-    (println "highlight-jsons" old-highlight-json)
-    (println "highlight-json" highlight-json)
+    #_#_#_(println "base-jsons" base-jsons)
+        (println "highlight-jsons" old-highlight-json)
+        (println "highlight-json" highlight-json)
 
 
     #_(println "old-state" old-state)
 
 
-
-
     (doseq [removed old-highlight-json]
       ;;(.log js/console "Removed: " removed)
-      (println "removed layer" removed)
+      #_(println "removed layer" removed)
       (.removeLayer leaflet removed))
 
 
     ; let's add the now highlighted json to state
     (reagent/set-state this {:jsons highlight-json})
 
+
+    ;L.circle([37.786542, -122.386022], {
+    ;          color: "red",
+    ;          fillColor: "#f03",
+    ;          fillOpacity: 0.5,
+    ;          radius: 50.0
+    ;      }).addTo(map);
+
+    (doseq [{:keys [lat lng name] :as layer-spec} stops]
+      (let [layer (js/L.circle (clj->js [lat
+                                         lng])
+                               300
+                               #js {:color       "black"
+                                    :fillColor   "white"
+                                    :fillOpacity 0.9
+                                    })]
+        (.addTo layer leaflet)
+        ))
+
+
+    ;L.popup({ elevation: 260.0 })
+    ;       .setLatLng([37.7952, -122.4028])
+    ;       .setContent("Transamerica Pyramid")
+    ;       .addTo(map);
+
+    (doseq [{:keys [lat lng name] :as layer-spec} stops]
+      (let [popup (js/L.popup #js {:elevation 260.0
+                                   :closeButton false
+                                   :closeOnClick false})]
+        (.setLatLng popup (clj->js [lat lng]))
+        (.setContent popup name)
+        (.addTo popup leaflet)))
+
     (doseq [{:keys [type url color linejoin weight] :as layer-spec} base-jsons]
-      #_(println "type" type)
       (let [layer (case type
                     :json (js/L.geoJson
                             (.parse js/JSON url)
@@ -134,12 +162,15 @@
                                       {:color    color
                                        :linejoin linejoin
                                        :weight   weight
-                                       :opacity  0.50}})))]
-        ;;(.log js/console "L.tileLayer = " layer)
+                                       :opacity  0.50}}))
+
+                  )]
         (.addTo layer leaflet)))
 
+
     (doseq [{:keys [type url color linejoin weight opacity] :as layer-spec} highlight-json]
-      #_(println "type" type)
+      (println "color" color)
+      (println "opacity" opacity)
       (let [layer (case type
                     :json (js/L.geoJson
                             (.parse js/JSON url)
@@ -149,7 +180,7 @@
                                        :weight   weight
                                        :opacity  opacity}})))]
         ;;(.log js/console "L.tileLayer = " layer)
-
+        #_(.bindPopUp (fn [layer] "test"))                  ;doesn't work
         (.addTo layer leaflet)))
 
     ))
@@ -164,10 +195,6 @@
     {:get-initial-state     (fn [_] {:mapspec mapspec})
      :component-did-mount   leaflet-did-mount
      :component-will-update leaflet-will-update
-
-     #_#_:component-will-update (fn [this old-argv]         ;; reagent provides you the entire "argv", not just the "props"
-                                  (let [new-argv (rest (reagent/argv this))]
-                                    (leaflet-will-update new-argv old-argv)
-                                    ))
-
      :render                leaflet-render}))
+
+
